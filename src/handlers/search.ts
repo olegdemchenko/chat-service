1729b4db5154953c199762a45cb2605198d3ce2d
OlegDemchenko <oleh.demchenko@sigma.software>
2024-04-01 14:56:07 +0300

    send search results via acknowledgements

--- src/handlers/search.ts
+++ src/handlers/search.ts
@@ -1,3 +1,4 @@
+import { Types } from "mongoose";
 import { RedisClientType } from "@redis/client";
 import { CustomSocket } from "../types";
 import UserModel from "../db/models/User";
@@ -6,18 +7,31 @@ export const handleFindUsers = (
   socket: CustomSocket,
   redisClient: RedisClientType,
 ) => {
-  socket.on("findUsers", async (nameFragment: string) => {
-    const match = await UserModel.find({
-      name: { $regex: new RegExp(nameFragment) },
-      externalId: { $ne: socket.data.user.externalId },
-    });
-    const activeUsers = await redisClient.SMEMBERS("active_users");
-    const usersWithStatuses = match.map(({ _id, name, email }) => ({
-      id: _id,
-      name,
-      email,
-      isOnline: activeUsers.includes(_id.toString()),
-    }));
-    socket.emit("foundUsers", usersWithStatuses);
-  });
+  socket.on(
+    "findUsers",
+    async (
+      nameFragment: string,
+      callback: (
+        foundUsers: {
+          id: Types.ObjectId;
+          name: string;
+          email: string | null;
+          isOnline: boolean;
+        }[],
+      ) => void,
+    ) => {
+      const match = await UserModel.find({
+        name: { $regex: new RegExp(nameFragment) },
+        externalId: { $ne: socket.data.user.externalId },
+      });
+      const activeUsers = await redisClient.SMEMBERS("active_users");
+      const usersWithStatuses = match.map(({ _id, name, email }) => ({
+        id: _id,
+        name,
+        email,
+        isOnline: activeUsers.includes(_id.toString()),
+      }));
+      callback(usersWithStatuses);
+    },
+  );
 };
